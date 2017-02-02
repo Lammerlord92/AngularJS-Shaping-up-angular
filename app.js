b25841f0575a471484fc6b4aa26962e877eef97d
Lammerlord92 <javcarfer92@gmail.com>
2017-02-02 12:55:52 +0100

    tabs inside out

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 
 (function functionName() {
   var app=angular.module('gemStore',[]);
+
+
   app.controller('StoreController',function(){
     this.products=gems;
   });
+
+  app.controller('PanelController',function(){
+    this.tab=1;
+    this.selectTab=function(setTab){
+      this.tab=setTab;
+    }
+    this.isSelected=function(checkTab){
+      return this.tab===checkTab;
+    };
+  });
+
 
   var gems=[{
     name:'Dodecahedron',
     price:2.95,
     description:'. . .',
-    canPurchase:true,
-    soldOut:false
+    images:[
+      "dodecahedron.jpg"
+    ]
   },{
     name:'Pentagonal Gem',
     price:5.95,
     description:'. . .',
-    canPurchase:false,
-    soldOut:false
+    images:[{
+      full:'dodecahedron.jpg',
+    }]
   }
 ]
 })();
