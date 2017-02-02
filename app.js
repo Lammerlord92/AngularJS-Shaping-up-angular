c7083ac6c3fd069c8d2427d5c24c8698fb75d1ff
Lammerlord92 <javcarfer92@gmail.com>
2017-02-02 18:37:47 +0100

    Formularios básicos

--- app.js
+++ app.js
@@ -11,10 +11,19 @@
     this.tab=1;
     this.selectTab=function(setTab){
       this.tab=setTab;
-    }
+    };
     this.isSelected=function(checkTab){
       return this.tab===checkTab;
     };
+  });
+
+  app.controller("ReviewController",function(){
+    this.review={};
+
+    this.addReview=function(product){
+      product.reviews.push(this.review);
+      this.review={};
+    }
   });
 
 
@@ -24,14 +33,16 @@
     description:'. . .',
     images:[
       "dodecahedron.jpg"
-    ]
+    ],
+    reviews:[]
   },{
     name:'Pentagonal Gem',
     price:5.95,
     description:'. . .',
     images:[{
       full:'dodecahedron.jpg',
-    }]
+    }],
+    reviews:[]
   }
 ]
 })();
